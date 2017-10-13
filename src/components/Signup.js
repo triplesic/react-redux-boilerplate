1125f89f5fee98a91635c463494d8dd352ebf154
Napasin Yamwat <triplesic@gmail.com>
2017-10-13 14:16:20 +0700

    Add client validation for signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,13 +11,64 @@ class Signup extends Component {
             username: '',
             password: '',
             confirmPassword: '',
-            name: ''
+            name: '',
+            isLoading: false,
+            errors: {}
+        }
+    }
+    // validatePassword() {
+    //     let passWordErr = {}
+
+    //     if (!_.isEqual(this.state.password, this.state.confirmPassword)) {
+    //         errors.password = "Password not match!"
+    //         errors.confirmPassword = "Password not match!"
+    //     }
+
+    //     return {
+    //         passWordErr,
+    //         isPasswordValid: _.isEmpty(passWordErr)
+    //     }
+    // }
+
+    isValid() {
+        let errors = {}
+
+        if (_.isEmpty(this.state.username)) {
+            errors.username = "This field is required!"
+        }
+        if (_.isEmpty(this.state.password)) {
+            errors.password = "This field is required!"
+        }
+        if (_.isEmpty(this.state.confirmPassword)) {
+            errors.confirmPassword = "This field is required!"
+        }
+        if (_.isEmpty(this.state.name)) {
+            errors.name = "This field is required!"
+        }
+
+        if (!_.isEqual(this.state.password, this.state.confirmPassword)) {
+            errors.password = "Password not match!"
+            errors.confirmPassword = "Password not match!"
+        }
+        // passwordValidate = validatePassword()
+
+        // if (validatePassword().isValid)
+        // {
+
+        // }
+        return {
+            errors,
+            isValid: _.isEmpty(errors)
         }
     }
 
     signup() {
-        console.log('sign up call')
-        console.log(this.state)
+        const { errors, isValid } = this.isValid()
+        if (!isValid) {
+            this.setState({ errors })
+        } else {
+            this.setState({ errors: {}, isLoading: true })
+        }
     }
 
     render() {
@@ -30,6 +81,7 @@ class Signup extends Component {
                             value=''
                             lableName='Username'
                             hintText='username'
+                            errorText={this.state.errors.username}
                             onChange={e => this.setState({ username: e.target.value })}
                         />
                     </div>
@@ -39,6 +91,7 @@ class Signup extends Component {
                             lableName='Password'
                             hintText='password'
                             type='password'
+                            errorText={this.state.errors.password}
                             onChange={e => this.setState({ password: e.target.value })}
                         />
                     </div>
@@ -48,6 +101,7 @@ class Signup extends Component {
                             lableName='Confirm password'
                             hintText='confirm password'
                             type='password'
+                            errorText={this.state.errors.confirmPassword}
                             onChange={e => this.setState({ confirmPassword: e.target.value })}
                         />
                     </div>
@@ -56,15 +110,19 @@ class Signup extends Component {
                             value=''
                             lableName='Name'
                             hintText='Name Lastname'
+                            errorText={this.state.errors.name}
                             onChange={e => this.setState({ name: e.target.value })}
                         />
                     </div>
                     <div className='col-md-12'>
-                        <RaisedButton label="Sign up" primary={true} className='common-button'
+                        <RaisedButton
+                            label="Sign up"
+                            primary={true}
+                            className='common-button'
+                            disabled={this.state.isLoading}
                             onClick={this.signup.bind(this)} />
                     </div>
                 </div>
-
             </div>
         )
     }
